717bea6a81ec3f85f4b7a3053805a523cc684e5d
maike soares <maikesoares2212@gmail.com>
2020-11-15 21:06:56 +0000

    Trabalho 1.1

--- script.js
+++ script.js
@@ -1,17 +1,53 @@
 
+var pessoas, index;
 function gravaPessoa(nome, email, telefone){
   
-  var tb= document.getElememtById("tbPessoas");
-  var qtdeLinhas= tb.rows.length;
-  var linha= tb.insertRow(qtdeLinhas);
+  pessoas =document.getElementById("tbPessoas");
   
-  var celID= linha.insertcell(0)
-  var cellNome= linha.insertcell(1);
-  var cellEmail= linha.insertcell(2);
-  var cellTelefone= linha.insertcell(3);
+  var qtdeLinhas= pessoas.rows.length;
+  var linha= pessoas.insertRow(qtdeLinhas);
   
+  var cellID= linha.insertCell(0);
+  var cellNome= linha.insertCell(1);
+  var cellEmail= linha.insertCell(2);
+  var cellTelefone= linha.insertCell(3);
+  
+  cellID.innerHTML=qtdeLinhas;
   cellNome.innerHTML= nome;
   cellEmail.innerHTML= email;
   cellTelefone.innerHTML=telefone;
   
+  percorrerLinhas();
+  
+}
+
+function percorrerLinhas(){
+  
+  for(var i=0; i< pessoas.rows.length; i++){
+    
+    pessoas.rows[i].onclick= function(){
+      
+      index =this.rowIndex;
+      document.getElementById("txtID").value=pessoas.rows[index].cells[0].innerText;
+      document.getElementById("txtNome").value=pessoas.rows[index].cells[1].innerText;
+      document.getElementById("txtEmail").value=pessoas.rows[index].cells[2].innerText;
+      document.getElementById("txtTelefone").value=pessoas.rows[index].cells[3].innerText;
+      
+      
+    }
+    
+  }
+  
+}
+
+function excluiDados(){
+  
+  for(var i=0; i< pessoas.rows.length; i++) {
+    
+    if(index == i){
+      
+      pessoas.deleteRow(index);
+      
+    }
+  }
 }
